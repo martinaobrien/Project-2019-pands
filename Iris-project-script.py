fdfdde8fe4c57bb7053d3bd8a6a79eded6d8666c
martinaobrien <G00376463@gmit.ie>
2019-04-27 13:13:59 +0100

    Create Iris-project-script.py
    Programe Script for project

---
+++ Iris-project-script.py
@@ -0,0 +1,14 @@
+# Martina O'Brien - 22- 04-2019
+# Programme and Scripting Project 2019 - Iris Data Set 
+
+import pandas as pd 
+import numpy as np
+
+iris_data = pd.read_csv("Iris-data-set.csv") #setting up a variable for use in the program
+
+print('Shape of the Iris Dataset')
+print(' ')
+print(iris_data.shape)
+
+
+
