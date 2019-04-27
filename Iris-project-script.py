e48509cc935773078a26ea2e9cf030655653bc9e
martinaobrien <G00376463@gmit.ie>
2019-04-27 13:45:26 +0100

    Update Iris-project-script.py
    Overview of high level statistics for the project - summaries of mean, use of shape and describe functions

--- Iris-project-script.py
+++ Iris-project-script.py
@@ -6,9 +6,38 @@ import numpy as np
 
 iris_data = pd.read_csv("Iris-data-set.csv") #setting up a variable for use in the program
 
-print('Shape of the Iris Dataset')
+print ("Investagations on Iris Dataset")
+
+print(' ')
+print('------------------------------------------------------------------------------------------------------------')
+print(' ')
+
+print('Shape of the Iris Dataset - number of individual sampls and the number of charateristics contained in the set') # gives the highest level of information about the date. Sourced: https://stackoverflow.com/questions/10200268/what-does-shape-do-in-for-i-in-rangey-shape0
 print(' ')
 print(iris_data.shape)
+
+print(' ')
+print('------------------------------------------------------------------------------------------------------------')
+print(' ')
+
+print('Iris Median')
+print(' ')
+print(iris_data.median())
+
+print(' ')
+print('------------------------------------------------------------------------------------------------------------')
+print(' ')
+
+print ("Summary of Key Statistics from Iris Dataset")
+print (iris_data.describe())
+
+print(' ')
+print('------------------------------------------------------------------------------------------------------------')
+print(' ')
+
+
+
+
 
 
 
