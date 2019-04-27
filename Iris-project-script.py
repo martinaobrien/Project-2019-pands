378bf19e3d8425c4349f0e62b36c2420fe1db396
martinaobrien <G00376463@gmit.ie>
2019-04-27 23:45:24 +0100

    Update Iris-project-script.py

--- Iris-project-script.py
+++ Iris-project-script.py
@@ -3,8 +3,13 @@
 
 import pandas as pd 
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 iris_data = pd.read_csv("Iris-data-set.csv") #setting up a variable for use in the program
+
+iris_data.columns = ['sepal length', 'sepal width', 'petal length', 'petal width', 'iris']
+print(' ')
 
 print ("Investagations on Iris Dataset")
 
@@ -12,7 +17,7 @@ print(' ')
 print('------------------------------------------------------------------------------------------------------------')
 print(' ')
 
-print('Shape of the Iris Dataset - number of individual sampls and the number of charateristics contained in the set') # gives the highest level of information about the date. Sourced: https://stackoverflow.com/questions/10200268/what-does-shape-do-in-for-i-in-rangey-shape0
+print('Shape of the Iris Dataset')# Provides an overview of the data captured and potential outputs of further investigation Sourced: https://stackoverflow.com/questions/10200268/what-does-shape-do-in-for-i-in-rangey-shape0
 print(' ')
 print(iris_data.shape)
 
@@ -20,7 +25,22 @@ print(' ')
 print('------------------------------------------------------------------------------------------------------------')
 print(' ')
 
-print('Iris Median')
+print ('Sample of Data contained in the Iris Dataset')# random sample of the dataset. source: https://pandas.pydata.org/pandas-docs/version/0.17.0/generated/pandas.DataFrame.sample.html
+print (iris_data.sample (5))
+x = iris_data.sample(5)
+
+print(' ')
+print('------------------------------------------------------------------------------------------------------------')
+print(' ')
+
+print ("Summary of Key Statistics from Iris Dataset") # generates statistical analysis of the features of the measures of centre points and distribution of values source:https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.describe.html
+print (iris_data.describe())
+
+print(' ')
+print('------------------------------------------------------------------------------------------------------------')
+print(' ')
+
+print('Iris Median') #this is an additional analysis conducted on the centre points of the data source: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.median.html
 print(' ')
 print(iris_data.median())
 
@@ -28,16 +48,39 @@ print(' ')
 print('------------------------------------------------------------------------------------------------------------')
 print(' ')
 
-print ("Summary of Key Statistics from Iris Dataset")
-print (iris_data.describe())
+df1 = iris_data[iris_data['iris'].str.contains("Iris-setosa")] 
+df1_mean = df1.mean()
+df1_describe = df1.describe()
+print('Iris-setosa mean')
+print(' ')
+print(df1_mean)
 
 print(' ')
 print('------------------------------------------------------------------------------------------------------------')
 print(' ')
 
+print ('Maximum, Minimum and Mode of the Dataset')
+df = iris_data
+print (df.max()) # will return max value of each column
+print (df.min())
+print (df.mode(0,4)) # will return max value of each row
+
+#title="Compare the Distributions of Sepal Width"
+#sns.boxplot(x="iris", y="sepal width", data=iris_data)
+# increasing font size
+#plt.title(title, fontsize=26)
+# Show the plot
+#plt.show()
 
 
 
-
-
-
+#for i in iris_data['iris'].unique():
+#build_list = iris_data['iris'] == i
+#species = iris_data[build_list]
+#print('\n', i, '\n', species.describe(), '\n')
+    #for j in ['sepal length', 'sepal width', 'petal length', 'petal width']:
+       # title = i
+        #plt.title(title)
+        #x = species[j]
+        #sns.distplot(x)
+    #    plt.show()
