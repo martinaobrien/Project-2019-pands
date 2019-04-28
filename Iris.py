7f4eb05cdfb006c3b65bce5f91b118b13a77904d
martinaobrien <47950332+martinaobrien@users.noreply.github.com>
2019-04-28 21:21:57 +0100

    Delete Iris.py
    Delete old test file

--- Iris.py
+++
@@ -1,8 +0,0 @@
-# Martina O'Brien - 22- 04-2019
-# Iris Data Set
-
-import pandas as pd #importing the pandas module as part of the Anaconda package for use in this project
-import random
-data = pd.read_csv("Iris-data-set.csv") #data set on Iris Flowers (Fisher:1936), this is imported into Git Hub
-
-print (data.describe())
